1974282c57adeb4f2cf89cca2fc98dcc1f2b0fac
Ludovic Riviere <ludovic.riviere2@gmail.com>
2015-05-31 16:51:21 +0200

    Example added

--- SmoothBeacon/app/src/main/java/co/ludriv/smoothbeacon/sample/SampleActivity.java
+++ SmoothBeacon/app/src/main/java/co/ludriv/smoothbeacon/sample/SampleActivity.java
@@ -1,27 +1,59 @@
 package co.ludriv.smoothbeacon.sample;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import co.ludriv.smoothbeacon.R;
+import co.ludriv.smoothbeacon.client.SBIntent;
 
 
 public class SampleActivity extends ActionBarActivity {
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            String action = intent.getAction();
+
+            if (SBIntent.ON_ENTER_REGION_ACTION.equals(action)) {
+                //
+                // we are entering in region!
+                //
+            } else if (SBIntent.ON_EXIT_REGION_ACTION.equals(action)) {
+                //
+                // we are exiting region!
+                //
+            } else if (SBIntent.ON_CHANGE_PROXIMITY_ACTION.equals(action)) {
+                //
+                // we are changing proximity!
+                //
+            }
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
+        registerReceiver(mReceiver, SBIntent.getSBIntentFilter());
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_sample, menu);
         return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 
     @Override
